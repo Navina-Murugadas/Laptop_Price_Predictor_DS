491b1a6b37f642ab43cf6c776e2ba28bea129c10
Navina M <72821323+Navina-Murugadas@users.noreply.github.com>
2023-08-27 00:31:40 +0530

    Add files via upload

---
+++ Laptop_Price_Predictor/app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Importing the model
+pipe = pickle.load(open('pipe.pkl', 'rb'))
+df = pickle.load(open('df.pkl', 'rb'))
+
+st.title("LAPTOP PRICE PREDICTOR")
+
+#Brand
+company = st.selectbox('Brand', df['Company'].unique())
+
+#Type of Laptop
+type = st.selectbox('Type', df['TypeName'].unique())
+
+#Ram
+ram = st.selectbox('RAM (In GB)', [2,4,6,8,12,16,24,32,64])
+
+#Weight
+weight = st.number_input('Weight of the Laptop')
+
+#Touchscreen
+touchscreen =  st.selectbox('Touchscreen',  ['No', 'Yes'])
+
+#IPS Panel
+ips = st.selectbox('IPS Panel', ['No', 'Yes'])
+
+#Screen size
+screen_size = st.number_input('Screen Size')
+
+#Resolution
+resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
+
+#CPU
+cpu = st.selectbox('CPU Brand', df['Cpu Brand'].unique())
+
+#HDD
+hdd = st.selectbox('HDD (In GB)', [0, 128, 256, 512, 1024, 2048])
+
+#SSD
+ssd = st.selectbox('SDD (In GB)', [0, 8, 128, 256, 512, 1024])
+
+#GPU
+gpu = st.selectbox('GPU', df['Gpu Brand'].unique())
+
+#Operating System
+os = st.selectbox('OS', df['OS'].unique())
+
+if st.button('PREDICT PRICE'):
+    #Query
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+
+    ppi = (((X_res**2) + (Y_res**2))**0.5) / screen_size
+
+    query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+    query = query.reshape(1,12)
+    st.title("PREDICTED PRIZE = Rs. " + str(int(np.exp(pipe.predict(query)[0]))))
